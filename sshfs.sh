6f43a78cbe8a4689182809959bdf202188eaab15
vsko <vsko@email.ua>
2022-05-12 18:17:07 +0300

    add sshfs.sh new fixed

--- sshfs.sh
+++ sshfs.sh
@@ -1,45 +1,50 @@
 #!/usr/bin/env bash
 #
+# SSHFS auto mount tool
+#
+# SSHFS_OPTIONS: allow_other,default_permissions,nonempty,uid=1000,gid=1000
+#
 # ~/.sshfsrc file:
-# sites:vsko@192.168.0.200:/usr/local/www/sites/:/mnt/sites
-# storage:vsko@192.168.0.200:/usr/storage/:/mnt/storage:~/.ssh/id_rsa
+# label1:user@host:/path/to/remote:/path/to/local
+# label2:user@host:/path/to/remote:/path/to/local:/path/to/id_rsa
 #
 # ~/.ssh/config file:
-# Host 192.168.0.200
-# HostName 178.20.153.253
-# Port 22
-# User root
-# IdentityFile /root/.ssh/id_rsa
-#
-# command:
-# sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa user@host:/home/user/ /mnt/share
+# Host server
+#   HostName ip
+#   User root
+#   IdentityFile /root/.ssh/id_rsa
+#   AddKeysToAgent yes
 #
 
 SSHFS_RCFILE=~/.sshfsrc
-SSHFS_VERBOSE=3
-#SSHFS_OPTIONS="allow_other,default_permissions"
+SSHFS_SSH_CONFIG=~/.ssh/config
 SSHFS_OPTIONS="allow_other"
+SSHFS_VERBOSE=3
 
-for data in `cat ${SSHFS_RCFILE}`;
+for data in $(cat $SSHFS_RCFILE);
 do
   name=$(echo ${data} | cut -f1 -d:)
   server=$(echo ${data} | cut -f2 -d:)
   remote_dir=$(echo ${data} | cut -f3 -d:)
   local_dir=$(echo ${data} | cut -f4 -d:)
   identity_file=$(echo ${data} | cut -f5 -d:)
-  options=${SSHFS_OPTIONS}
+  params="-o ${SSHFS_OPTIONS}"
 
   if [[ ${data::1} == '#' ]]; then continue; fi
   if [[ $name == $server ]]; then continue; fi
   if [[ ! -z $identity_file ]];
   then
-   options+=",IdentityFile=~/.ssh/id_rsa"
+   params+=",IdentityFile=${identity_file}"
   fi
 
   if [[ ! -z $remote_dir && ! -z $local_dir ]];
   then
-    command=(sudo sshfs -o ${options} ${server}:${remote_dir} ${local_dir})
-    ${command[@]}
+    if [[ ! -z $SSHFS_SSH_CONFIG ]];
+    then
+      params="-F ${SSHFS_SSH_CONFIG} ${params}"
+    fi
+
+    command="sshfs ${server}:${remote_dir} ${local_dir} ${params}"
 
     if [[ $SSHFS_VERBOSE -gt 0 ]]; then
       if [[ $SSHFS_VERBOSE -eq 1 ]];
@@ -55,10 +60,13 @@ do
         fi
         if [[ $SSHFS_VERBOSE -gt 2 ]];
         then
-          echo "Command: ${command[@]}"
+          echo "Command: ${command}"
         fi
       fi
     fi
+
+    eval $command
+
   fi
 
 done
